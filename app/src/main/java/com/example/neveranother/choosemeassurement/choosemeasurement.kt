5e19cdb18fe1ccd0f1c089148bdae917df1f0e66
Rosalina <rosalinasgithub@gmail.com>
2026-05-19 13:10:34 +0200

    added button for 3D-scan

--- app/src/main/java/com/example/neveranother/choosemeassurement/choosemeasurement.kt
+++ app/src/main/java/com/example/neveranother/choosemeassurement/choosemeasurement.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,8 +17,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.neveranother.R
 
+
+val btn3DText: String = "3D-scan"
+
 @Composable
 fun Choosemeasurement(){
+Scan3D()
+}
+@Composable
+fun Scan3D(){
     Column (
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -33,9 +41,20 @@ fun Choosemeasurement(){
             Column (
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Text("This is choosemeasurement")
-                Text("Hello world")
+                FilledButtonExample {  }
             }
         }
+    }
+}
+
+
+@Composable
+fun FilledButtonExample(onClick: () -> Unit) {
+    Button(
+        modifier = Modifier.
+    clip(RoundedCornerShape(15.dp)),
+        onClick = { onClick() })
+    {
+        Text(btn3DText)
     }
 }
