5fb2c348db25552537466647e7d12b4058f43342
Rosalina <rosalinasgithub@gmail.com>
2026-05-19 11:55:16 +0200

    added picture for 3dscanner

--- app/src/main/java/com/example/neveranother/choosemeassurement/choosemeasurement.kt
+++ app/src/main/java/com/example/neveranother/choosemeassurement/choosemeasurement.kt
@@ -1,14 +1,41 @@
 package com.example.neveranother.choosemeassurement
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.neveranother.R
 
 @Composable
 fun Choosemeasurement(){
-    Text("This is choosemeasurement")
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+
+        Box{
+            Image(
+                painter = painterResource(id = R.drawable.threed),
+                contentDescription = "",
+                modifier = Modifier.clip(RoundedCornerShape(15.dp)),
+            )
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text("This is choosemeasurement")
+                Text("Hello world")
+            }
+        }
+    }
 }
