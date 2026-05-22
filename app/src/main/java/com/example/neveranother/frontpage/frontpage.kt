c6661921b62fa338f0bd6a66c41d6ca22f4564d3
Mathias <mathias.borup@hotmail.com>
2026-05-22 12:58:59 +0200

    Fixed navbar + start på footer.

--- app/src/main/java/com/example/neveranother/frontpage/frontpage.kt
+++ app/src/main/java/com/example/neveranother/frontpage/frontpage.kt
@@ -1,5 +1,6 @@
 package com.example.neveranother.frontpage
 
+import android.R.attr.padding
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -31,6 +32,10 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.layout.ContentScale
 
 val nohemiBlack = FontFamily(
@@ -38,37 +43,275 @@ val nohemiBlack = FontFamily(
 )
 val warmGrey = Color(0xFFF9F6EE)
 
+@Composable
+fun Navbar() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .background(Color.White.copy(alpha = 0.8f))
+            .padding(horizontal = 16.dp)
+    ) {
+
+        // Left icon
+        Icon(
+            painter = painterResource(id = R.drawable.dehaze_24px),
+            contentDescription = "Menu icon",
+            tint = Color.Black,
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .size(28.dp)
+        )
+
+        // Center logo
+        Icon(
+            painter = painterResource(id = R.drawable.logo_black),
+            contentDescription = "Nav bar logo",
+            modifier = Modifier
+                .align(Alignment.Center)
+                .height(32.dp)
+        )
+
+        // Right icon
+        Icon(
+            painter = painterResource(id = R.drawable.shopping_bag_24px),
+            contentDescription = "Shopping bag icon",
+            tint = Color.Black,
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .size(28.dp)
+        )
+    }
+}
 
 @Composable
 fun Frontpage() {
 
-    Column(
-        modifier = Modifier
-            .padding(0.dp)
-            .verticalScroll(rememberScrollState())
-    ) {
+    // Scaffold to make sure the Navbar is on top of other content.
+    Scaffold(
+        topBar = {
+            Navbar()
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(top = 0.dp) // IMPORTANT: remove top padding
+                .verticalScroll(rememberScrollState())
+        ) {
+            Box {
+                Image(
+                    painter = painterResource(id = R.drawable.bh_forsidebillede1),
+                    contentDescription = "Frontpage image 1"
+                )
 
-        Box {
-            Image(
-                painter = painterResource(id = R.drawable.bh_forsidebillede1),
-                contentDescription = "Frontpage image 1"
+                Text(
+                    text = "En BH tilpasset dine mål",
+                    fontFamily = nohemiBlack,
+                    fontSize = 26.sp,
+                    modifier =
+                        Modifier.padding(top = 245.dp, start = 15.dp),
+
+
+                    )
+            }
+            Box {
+                Image(
+                    painter = painterResource(id = R.drawable.bh_forsidebillede2),
+                    contentDescription = "Frontpage image 2"
+                )
+
+                Button(
+                    onClick = {
+                        // Do something
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = NAaccentColor
+                    ),
+                    modifier = Modifier
+                        .padding(top = 270.dp, start = 15.dp)
+                        .width(150.dp)
+                        .shadow(
+                            elevation = 3.dp,
+                            shape = CircleShape,
+                            ambientColor = Color.Black.copy(alpha = 0.3f),
+                            spotColor = Color.Black
+                        )
+                ) {
+                    Text(
+                        text = "Se produkt",
+                        fontSize = 20.sp
+                    )
+                }
+
+                Text(
+                    text = "Dansk design \nskræddersyet i Holland",
+                    fontFamily = nohemiBlack,
+                    fontSize = 26.sp,
+                    modifier =
+                        Modifier.padding(top = 330.dp, start = 15.dp)
+                )
+
+            }
+
+            Box {
+                Image(
+                    painter = painterResource(id = R.drawable.bh_forsidebillede3),
+                    contentDescription = "Frontpage image 3",
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillWidth
+                )
+
+
+                Text(
+                    text = "Sig farvel til standardstørrelser",
+                    fontFamily = nohemiBlack,
+                    fontSize = 26.sp,
+                    modifier =
+                        Modifier.padding(top = 310.dp, start = 15.dp)
+                )
+            }
+
+            /*
+        Text(
+            text = "Sig farvel til standardstørrelser",
+            fontFamily = nohemiBlack,
+            fontSize = 26.sp,
+            modifier = Modifier.padding(top = 40.dp, start = 15.dp)
+        )
+         */
+
+            Text(
+                text = "Traditionelle BH-størrelser har aldrig passet til alle kroppe.\n" +
+                        "\n" +
+                        "NEVER ANOTHER ændrer det med en digitalt skræddersyet, bøjlefri BH tilpasset ud fra dine egne mål. Den sømløse 3D-knit teknologi skaber en blødere og mere behagelig pasform, der føles helt som din egen.\n" +
+                        "\n" +
+                        "Tag dine mål derhjemme.",
+                //fontFamily = nohemiBlack,
+                fontSize = 16.sp,
+                modifier =
+                    Modifier.padding(top = 70.dp, start = 15.dp)
+            )
+            Text(
+                text = "Vi guider dig hele vejen.",
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+                modifier =
+                    Modifier.padding(top = 0.dp, start = 15.dp)
             )
 
             Text(
-                text = "En BH tilpasset dine mål",
+                text = "Når pasform føles \npersonlig",
                 fontFamily = nohemiBlack,
                 fontSize = 26.sp,
-                modifier =
-                    Modifier.padding(top = 245.dp, start = 15.dp),
-
-
+                modifier = Modifier.padding(top = 70.dp, start = 15.dp)
             )
-        }
-        Box {
-            Image(
-                painter = painterResource(id = R.drawable.bh_forsidebillede2),
-                contentDescription = "Frontpage image 2"
+            Text(
+                text = "Hvad vores kunder siger",
+                //fontFamily = nohemiBlack,
+                fontSize = 26.sp,
+                modifier = Modifier.padding(top = 0.dp, start = 15.dp)
             )
+
+            Row(
+                modifier = Modifier
+                    .horizontalScroll(rememberScrollState())
+            ) {
+
+                Column(
+                    modifier = Modifier
+                        .padding(15.dp, top = 40.dp)
+                        .background(
+                            color = warmGrey,
+                            shape = RoundedCornerShape(16.dp)
+                        )
+                        .padding(20.dp)
+                        .width(300.dp)
+                ) {
+
+                    Text(
+                        text = "Gitte",
+                        fontFamily = nohemiBlack,
+                        fontSize = 20.sp
+                    )
+
+                    Text(
+                        text = "Bh'en sidder helt perfekt på min krop. " +
+                                "Jeg er virkelig glad for den, og jeg føler mig meget veltilpas i den. " +
+                                "Stoffet og pasformen passer mig rigtig godt. " +
+                                "Algoritmen virker!",
+                        fontSize = 16.sp,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
+
+                Column(
+                    modifier = Modifier
+                        .padding(15.dp, top = 40.dp)
+                        .background(
+                            color = warmGrey,
+                            shape = RoundedCornerShape(16.dp)
+                        )
+                        .padding(20.dp)
+                        .width(300.dp)
+                ) {
+
+                    Text(
+                        text = "Teresa",
+                        fontFamily = nohemiBlack,
+                        fontSize = 20.sp
+                    )
+
+                    Text(
+                        text = "Denne bh er præcis som lovet. Den er blød, strammer ikke og føles ikke ubehagelig nogen steder, og jeg bemærker slet ikke, at jeg har en bh på, selvom jeg bruger en J-skål.",
+                        fontSize = 16.sp,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
+
+                Column(
+                    modifier = Modifier
+                        .padding(15.dp, top = 40.dp)
+                        .background(
+                            color = warmGrey,
+                            shape = RoundedCornerShape(16.dp)
+                        )
+                        .padding(20.dp, end = 60.dp)
+                        .width(300.dp)
+                ) {
+
+                    Text(
+                        text = "Birthe",
+                        fontFamily = nohemiBlack,
+                        fontSize = 20.sp
+                    )
+
+                    Text(
+                        text = "“This bra was made for you” stod der på pakken, da jeg modtog min bh fra Never Another. De har ret! Den er perfekt skræddersyet til mig, den løfter og samler som en bøjle-bh, og ’stof-bøjlen’ irriterer overhovedet ikke. Kan varmt anbefales.",
+                        fontSize = 16.sp,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
+                //Horisontal Row ending here
+            }
+
+            Text(
+                text = "Pasformsgaranti - test",
+                fontFamily = nohemiBlack,
+                fontSize = 26.sp,
+                modifier = Modifier.padding(top = 70.dp, start = 15.dp)
+            )
+
+            Column(
+                modifier = Modifier
+                    .padding(15.dp)
+                    .background(
+                        color = warmGrey,
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                    .padding(20.dp, end = 60.dp)
+                    .width(300.dp)
+                    .height(300.dp)
+            ) {}
 
             Button(
                 onClick = {
@@ -78,8 +321,8 @@ fun Frontpage() {
                     containerColor = NAaccentColor
                 ),
                 modifier = Modifier
-                    .padding(top = 270.dp, start = 15.dp)
-                    .width(150.dp)
+                    .padding(top = 0.dp, start = 15.dp, end = 15.dp)
+                    .fillMaxWidth()
                     .shadow(
                         elevation = 3.dp,
                         shape = CircleShape,
@@ -93,201 +336,38 @@ fun Frontpage() {
                 )
             }
 
-            Text(
-                text = "Dansk design \nskræddersyet i Holland",
-                fontFamily = nohemiBlack,
-                fontSize = 26.sp,
-                modifier =
-                    Modifier.padding(top = 330.dp, start = 15.dp)
-            )
-
-        }
-
-        Box {
-            Image(
-                painter = painterResource(id = R.drawable.bh_forsidebillede3),
-                contentDescription = "Frontpage image 3",
-                modifier = Modifier.fillMaxWidth(),
-                contentScale = ContentScale.FillWidth
-            )
-
-
-            Text(
-                text = "Sig farvel til standardstørrelser",
-                fontFamily = nohemiBlack,
-                fontSize = 26.sp,
-                modifier =
-                    Modifier.padding(top = 310.dp, start = 15.dp)
-            )
-        }
-
-        /*
-        Text(
-            text = "Sig farvel til standardstørrelser",
-            fontFamily = nohemiBlack,
-            fontSize = 26.sp,
-            modifier = Modifier.padding(top = 40.dp, start = 15.dp)
-        )
-         */
-
-        Text(
-            text = "Traditionelle BH-størrelser har aldrig passet til alle kroppe.\n" +
-                    "\n" +
-                    "NEVER ANOTHER ændrer det med en digitalt skræddersyet, bøjlefri BH tilpasset ud fra dine egne mål. Den sømløse 3D-knit teknologi skaber en blødere og mere behagelig pasform, der føles helt som din egen.\n" +
-                    "\n" +
-                    "Tag dine mål derhjemme.",
-            //fontFamily = nohemiBlack,
-            fontSize = 16.sp,
-            modifier =
-                Modifier.padding(top = 70.dp, start = 15.dp)
-        )
-        Text(
-            text = "Vi guider dig hele vejen.",
-            fontWeight = FontWeight.Bold,
-            fontSize = 16.sp,
-            modifier =
-                Modifier.padding(top = 0.dp, start = 15.dp)
-        )
-
-        Text(
-            text = "Når pasform føles \npersonlig",
-            fontFamily = nohemiBlack,
-            fontSize = 26.sp,
-            modifier = Modifier.padding(top = 70.dp, start = 15.dp)
-        )
-        Text(
-            text = "Hvad vores kunder siger",
-            //fontFamily = nohemiBlack,
-            fontSize = 26.sp,
-            modifier = Modifier.padding(top = 0.dp, start = 15.dp)
-        )
-
-        Row(
-            modifier = Modifier
-                .horizontalScroll(rememberScrollState())
-        ) {
-
             Column(
                 modifier = Modifier
-                    .padding(15.dp, top = 40.dp)
-                    .background(
-                        color = warmGrey,
-                        shape = RoundedCornerShape(16.dp)
-                    )
-                    .padding(20.dp)
-                    .width(300.dp)
+                    .padding(top = 10.dp)
+                    .background(color = Color.Red.copy(alpha = 0.5f))
+                    .fillMaxWidth()
             ) {
 
-                Text(
-                    text = "Gitte",
-                    fontFamily = nohemiBlack,
-                    fontSize = 20.sp
+                Icon(
+                    painter = painterResource(id = R.drawable.logo_black),
+                    contentDescription = "Nav bar logo",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 15.dp, end = 15.dp))
+
+                Text (
+                    text = "Kontakt",
+                    fontSize = 16.sp
                 )
 
                 Text(
-                    text = "Bh'en sidder helt perfekt på min krop. " +
-                            "Jeg er virkelig glad for den, og jeg føler mig meget veltilpas i den. " +
-                            "Stoffet og pasformen passer mig rigtig godt. " +
-                            "Algoritmen virker!",
-                    fontSize = 16.sp,
-                    modifier = Modifier.padding(top = 10.dp)
+                    text = "E-mail: Customercare@neveranother.dk",
+                    fontSize = 16.sp
+                )
+
+                Text(
+                    text = "Never Another ApS, CVR: 43577662",
+                    fontSize = 16.sp
                 )
             }
 
-            Column(
-                modifier = Modifier
-                    .padding(15.dp, top = 40.dp)
-                    .background(
-                        color = warmGrey,
-                        shape = RoundedCornerShape(16.dp)
-                    )
-                    .padding(20.dp)
-                    .width(300.dp)
-            ) {
-
-                Text(
-                    text = "Teresa",
-                    fontFamily = nohemiBlack,
-                    fontSize = 20.sp
-                )
-
-                Text(
-                    text = "Denne bh er præcis som lovet. Den er blød, strammer ikke og føles ikke ubehagelig nogen steder, og jeg bemærker slet ikke, at jeg har en bh på, selvom jeg bruger en J-skål.",
-                    fontSize = 16.sp,
-                    modifier = Modifier.padding(top = 10.dp)
-                )
-            }
-
-            Column(
-                modifier = Modifier
-                    .padding(15.dp, top = 40.dp)
-                    .background(
-                        color = warmGrey,
-                        shape = RoundedCornerShape(16.dp)
-                    )
-                    .padding(20.dp, end = 60.dp)
-                    .width(300.dp)
-            ) {
-
-                Text(
-                    text = "Birthe",
-                    fontFamily = nohemiBlack,
-                    fontSize = 20.sp
-                )
-
-                Text(
-                    text = "“This bra was made for you” stod der på pakken, da jeg modtog min bh fra Never Another. De har ret! Den er perfekt skræddersyet til mig, den løfter og samler som en bøjle-bh, og ’stof-bøjlen’ irriterer overhovedet ikke. Kan varmt anbefales.",
-                    fontSize = 16.sp,
-                    modifier = Modifier.padding(top = 10.dp)
-                )
-            }
-            //Horisontal Row ending here
         }
-
-        Text(
-            text = "Pasformsgaranti - test",
-            fontFamily = nohemiBlack,
-            fontSize = 26.sp,
-            modifier = Modifier.padding(top = 70.dp, start = 15.dp)
-        )
-
-        Column(
-            modifier = Modifier
-                .padding(15.dp)
-                .background(
-                    color = warmGrey,
-                    shape = RoundedCornerShape(16.dp)
-                )
-                .padding(20.dp, end = 60.dp)
-                .width(300.dp)
-                .height(300.dp)
-        ){}
-
-        Button(
-            onClick = {
-                // Do something
-            },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = NAaccentColor
-            ),
-            modifier = Modifier
-                .padding(top = 0.dp, start = 15.dp)
-                .fillMaxWidth()
-                .shadow(
-                    elevation = 3.dp,
-                    shape = CircleShape,
-                    ambientColor = Color.Black.copy(alpha = 0.3f),
-                    spotColor = Color.Black
-                )
-        ) {
-            Text(
-                text = "Se produkt",
-                fontSize = 20.sp
-            )
-        }
-
     }
-
 }
 
 @Preview(showBackground = true)
