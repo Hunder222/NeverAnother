7f5526624da45fb0600142f118b7af37f4af0860
Mathias <mathias.borup@hotmail.com>
2026-05-22 13:01:44 +0200

    t32

--- app/src/main/java/com/example/neveranother/frontpage/frontpage.kt
+++ app/src/main/java/com/example/neveranother/frontpage/frontpage.kt
@@ -1,6 +1,5 @@
 package com.example.neveranother.frontpage
 
-import android.R.attr.padding
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
